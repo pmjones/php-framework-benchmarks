5ea9a3e2b503bb2c37fafc16163c23798bc2f6e7
Paul M. Jones <pmjones88@gmail.com>
2011-01-22 12:33:19 -0600

    add cake 1.3.7 to the mix

---
+++ htdocs/cake/1.3.7/app/controllers/hello_controller.php
@@ -0,0 +1,11 @@
+<?php
+class HelloController extends AppController {
+    var $layout = null;
+    var $autoLayout = false;
+    var $uses = array();
+    var $helpers = array();
+    function index()
+    {
+        
+    }
+}
