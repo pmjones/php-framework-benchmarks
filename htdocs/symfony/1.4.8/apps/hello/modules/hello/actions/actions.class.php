3ea7514b6e9faa29670b232d2094833d1db472be
Paul M. Jones <pmjones88@gmail.com>
2011-01-22 14:06:20 -0600

    add symfony/1.4.8 hello world module

---
+++ htdocs/symfony/1.4.8/apps/hello/modules/hello/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * hello actions.
+ *
+ * @package    hello
+ * @subpackage hello
+ * @author     Paul M. Jones
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class helloActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
