49a86c2e48c8001dd5b40ee70d5dbe13c74217c8
Paul M. Jones <pmjones88@gmail.com>
2011-02-16 16:05:37 -0600

    update cake 1.3.7 to turn off components, per notes from PhpNut

--- htdocs/cake/1.3.7/app/controllers/hello_controller.php
+++ htdocs/cake/1.3.7/app/controllers/hello_controller.php
@@ -1,11 +1,11 @@
 <?php
 class HelloController extends AppController {
-    var $layout = null;
-    var $autoLayout = false;
-    var $uses = array();
-    var $helpers = array();
-    function index()
-    {
-        
-    }
+  var $layout = null;
+  var $autoLayout = false;
+  var $uses = array();
+  var $helpers = false;
+  var $components = false;
+  
+  function index() {
+  }
 }
