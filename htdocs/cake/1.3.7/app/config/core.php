c4d31532ef9a8a8eb4a1be047bd56d0d4b1124e7
Paul M. Jones <pmjones88@gmail.com>
2011-02-17 15:47:12 -0600

    putting cake back to file-cache; none of the others use APC for caching, so not fair to do that for cake

--- htdocs/cake/1.3.7/app/config/core.php
+++ htdocs/cake/1.3.7/app/config/core.php
@@ -300,10 +300,4 @@
  *	));
  *
  */
-  // Cache::config('default', array('engine' => 'File'));
- 	Cache::config('default', array(
-		'engine' => 'Apc', //[required]
-		'duration'=> 3600, //[optional]
-		'probability'=> 100, //[optional]
-    // 'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
-	));
+  Cache::config('default', array('engine' => 'File'));
