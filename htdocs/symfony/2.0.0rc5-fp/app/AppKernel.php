be54b36f4299598f5273fde85d27f53c233190d9
Paul M. Jones <pmjones88@gmail.com>
2011-07-23 10:01:51 -0500

    complete optimized version of rc5

--- htdocs/symfony/2.0.0rc5-fp/app/AppKernel.php
+++ htdocs/symfony/2.0.0rc5-fp/app/AppKernel.php
@@ -9,23 +9,23 @@ class AppKernel extends Kernel
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            // new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            // new Symfony\Bundle\TwigBundle\TwigBundle(),
+            // new Symfony\Bundle\MonologBundle\MonologBundle(),
+            // new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            // new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
+            // new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            // new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Acme\HelloBundle\AcmeHelloBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
+        // if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        //     $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+        //     $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+        //     $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+        //     $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        // }
 
         return $bundles;
     }
