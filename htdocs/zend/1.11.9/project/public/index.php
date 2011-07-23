c625a5818723ba48c04450e871f56c430980a07f
Paul M. Jones <pmjones88@gmail.com>
2011-07-23 15:25:29 -0500

    update include path

--- htdocs/zend/1.11.9/project/public/index.php
+++ htdocs/zend/1.11.9/project/public/index.php
@@ -10,7 +10,7 @@ defined('APPLICATION_ENV')
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
+    realpath(APPLICATION_PATH . '/../../library'),
     get_include_path(),
 )));
 
